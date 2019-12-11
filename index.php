c9bde01cbaab63826da317e82fd7a76c64b9c76f
Mariano_Flores <marianobc12@hotmail.com>
2019-12-11 18:10:58 -0300

    Subida del proyecto a github

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Inicio</title>
+</head>
+<body>
+    
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
+<!-- PARA EMPEZAR CON ESTOS ENLACES ESTA BIEN , TIENE JQUERY , BOOTSTRAP , LOS ICONOS DE FONT-AWESOME , LAS ANIMACIONES DE ANIMATED CSS , Y HAY UN ARCHIVO ESTILO.CSS 
+    QUE AHI PONEMOS EL CSS NUESTRO
+-->
+
+<!-- EN ESTA PAGINA TENDRIA QUE IR EL MAQUETADO QUE HICISTE -->
+
+<!-- TESTEE LOS ARCHIVOS INTEGRADOS Y NO ME DIO NINGUNA FALLA EN LA CONSOLA DEL NAVEGADOR... ASI QUE  ESTAN BIEN LINKEADOS -->
+
