6365805c3decd866a0c6df5f8587c94514e92765
desqui92 <desqui92@gmail.com>
2019-12-12 02:56:54 -0300

    Avance1

--- index.php
+++ index.php
@@ -10,10 +10,30 @@
     <title>Inicio</title>
 </head>
 <body style="background: url('img/fondo1.jpg') no-repeat center center fixed; background-size:cover; width:100%; height:100%">
-<div class="container">
-<div class="row"></div>
-<div class="col-md-6"></div>
-</div>    
+  <div class="container" style="text-align:center;">
+    <div class="row">
+      <div class="col-sm-8">
+        <div style="height: 80%; width:80%;background-color:white; margin-top:10%;border-radius:10px;">
+          <div style="text-align:center;">
+             <img src="img/logo.png" class="logo">
+          </div>
+          <hr style="border-top: 1px solid #B7B7B7; width:95%; border-radius:5px;">
+          <form class="col-sm-12">
+            <span style="color:#112A7A; font-size:30px;">Iniciar Sesi&oacute;n</span>
+              <div class="row justify-content-md-center">
+                <div class="form-group col-sm-10 col-sm-offset-1">
+                  <label><span style="color:#112A7A; font-size:30px;">Usuario</span></label><br>
+                  <input type="text" class="form-usuar" placeholder="Ingresar usuario..."><br>
+                  <label><span style="color:#112A7A; font-size:30px;">Contrase&ntilde;a</span></label><br>
+                  <input type="password" style="padding-top:10px;" placeholder="Ingresar contrase&ntilde;a...">
+                </div>
+              </div>
+          </form>
+        </div>
+      </div>  
+    </div>
+  </div>
+
 
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
